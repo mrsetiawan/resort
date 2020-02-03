24ca8fca0079b6b6822b5d6c8a35e4e4e6b092ba
rahmat <muhamadrahmatsetiawan@gmail.com>
2020-02-03 18:18:54 +0700

    fetch data

--- src/context.js
+++ src/context.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react'
+import items from './data'
 
 const RoomContext = React.createContext();
 
 class RoomProvider extends Component {
 
   state = {
-    name: 'muhamad rahmat setiawan',
-    greeting :'hi good morning'
+    rooms:[],
+    sortedRooms:[],
+    featuredRooms:{},
+    loading:true
+  }
+
+  componentDidMount(){
+    let rooms = this.handleData(items)
+    let featuredRooms = rooms.filter(room => room.fields == true)
+    console.log(featuredRooms)
+    this.setState({
+      rooms,
+      featuredRooms,
+      sortedRooms:rooms,
+      loading:false
+    })
+  }
+
+  handleData = items => {
+    let allData = items.map(item => {
+      let id = item.sys.id
+      let image = item.fields.images.map(image => image.fields.file.url)
+      let room = {...item.fields, image, id}
+      return room
+    });
+    return allData
   }
 
   render() {
+    console.log(this.state.sortedRooms)
     return (
       <RoomContext.Provider value={{...this.state}}>
         {this.props.children}
